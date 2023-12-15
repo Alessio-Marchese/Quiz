c1a29955ce5e323963620872393b974dde3b6ab0
Alessio <Alessio@Portatile>
2023-12-15 12:35:10 +0100

    Fix cannot insantiate servlet

--- src/main/java/it/generationitaly/quizapp/controller/LoginServlet.java
+++ src/main/java/it/generationitaly/quizapp/controller/LoginServlet.java
@@ -6,9 +6,9 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.RequestDispatcher;
 
 import java.io.IOException;
-import java.lang.ProcessBuilder.Redirect;
 
 import it.generationitaly.quizapp.entity.Utente;
 import it.generationitaly.quizapp.repository.UtenteRepository;
@@ -16,28 +16,29 @@ import it.generationitaly.quizapp.repository.impl.UtenteRepositoryImpl;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
-       
+
 	private UtenteRepository utenteRepository = new UtenteRepositoryImpl(Utente.class);
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		
+
 		Utente utente = utenteRepository.findByUsername(username);
-		
+
 		if (utente != null && utente.getPassword() == password) {
-			
+
 			HttpSession session = request.getSession();
 			session.setAttribute("utenteId", utente.getId());
 			session.setAttribute("username", utente.getUsername());
-			
-			response.sendRedirect("welcomeQuizApp.jsp");
-			
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
+			requestDispatcher.forward(request, response);
+
 		} else {
 			response.sendRedirect("login.jsp?errore");
 		}
-		
+
 	}
 
 }
